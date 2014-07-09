ff0def6d583513216b34ad43a9fda6d5fde5d173
Albert Yang <ayoungbest@gmail.com>
2014-07-09 14:01:02 -0400

    started implementing Parser and added test log file

--- maillex.go
+++ maillex.go
@@ -4,23 +4,46 @@ import (
 	"fmt"
 //	"regexp"
 	"os"
-	
+	"io/ioutil"
+	"strings"
 )
 
-const usageMsg string = "usage: gomaillex maillog[filename] -option\n"
+const usageMsg string = "usage: gomaillex maillog[filename] -option\n" + 
+						"-option: -s [summary]\n"
 
 func usage() {
 	fmt.Printf(usageMsg)
-
 	os.Exit(1)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// Parse the selected log file 
+// And obtain the information needed
+func parser(file string) {
+    dat, err := ioutil.ReadFile(file)
+    check(err)
+ //   fmt.Print(string(dat))
+    fmt.Print("\n")
+
+    var data []string = strings.Split(string(dat), "\n")
+    fmt.Printf("num of lines: %d\n", len(data))
 }
 
 func main() {
 	args := os.Args
 	fmt.Printf("\n")
+
 	if len(args) < 2 {
 		usage()
 	}
+	var file string = args[1]
+	parser(file)
+
 	for i := 0; i < len(args); i++ {
 		fmt.Printf("%s\n", args[i])
 	}
